f76117a5d39fe0d289ecb9a98680e67348962a39
Mike Rae <mikerae@me.com>
2022-10-25 20:07:24 +0000

    Add api throttling error handling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -65,6 +65,10 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                const resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(
+                    `<h4>Too many requests, please wait until ${resetTime.toLocaleDateString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
