e9c289d4db71afcc821450f85174b48bbeff663b
Mike Rae <mikerae@me.com>
2022-11-20 15:47:57 +0000

    Fix bug in map.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,34 +4,37 @@ function initMap() {
         center: {
             lat: 46.619261,
             lng: -33.134766
+
         }
     });
     const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    const markerCluster = new MarkerClusterer({
-        map,
-        markers
-    });
-
     const locations = [{
             lat: 40.785091,
-            lng: -73.9682856
+            lng: -73.968285,
         },
         {
             lat: 41.084045,
-            lng: -73.874245
+            lng: -73.874245,
         },
         {
             lat: 40.754932,
-            lng: -73.984016
+            lng: -73.984016,
         },
     ];
 
     const markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
-            label: labels[i % labels.length]
+            label: labels[i % labels.length],
         });
+    });
+
+    console.log(markers);
+
+    const markerCluster = new markerClusterer.MarkerClusterer({
+        map,
+        markers
     });
 
 }
